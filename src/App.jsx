5b196952b6bc109625439c841acded62b69d4234
Jobwafula <jobwafulabg@gmail.com>
2023-10-25 18:33:45 +0300

    cha

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Contact from './Contact'
-import Sidecontent from './components/sidecontent'
+
 import Home from './components/Home'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { BrowserRouter,Routes,Route} from 'react-router-dom'
+import Contact from './Contact'
 
 function App() {
   
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element:<Home />
-  },{
-    path:"contact",
-    element:<Contact />
-  }
-])
+ 
   return (
     <>
-     <RouterProvider router={router}/>
+    <BrowserRouter>
+    <Routes>
+      <Route  path='/' element={<Home />}/>
+      <Route path='contact' element={<Contact />} />
+    </Routes>
+    </BrowserRouter>
        
     </>
   )
